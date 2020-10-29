dda5efb683ccc152aa01b71684902289e4f6d9d4
DESKTOP-QKJ565Q\Nestor Puentes <nestorpuentesin@gmail.com>
2020-10-29 01:32:29 -0600

    Se empezo a desarrollar las API (funciones en los Controladores y Rutas API, solo llevo el GET de comentarios)

--- app/Http/Controllers/API/CommentController.php
+++ app/Http/Controllers/API/CommentController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class CommentController extends Controller
 {
-    //
+    public function index($id=null){
+        
+        $comment = DB::table('comments')->join('users','comments.user_id','users.id')->join('products','comments.product_id','products.id')->select('name as Usuario','product as Producto','comment as Comentario')->where('comments.id',$id)->get(); 
+
+        $comments = DB::table('comments')->join('users','comments.user_id','users.id')->join('products','comments.product_id','products.id')->select('name as Usuario','product as Producto','comment as Comentario')->get();
+
+        if($id)
+            return response()->json(["Comentario: ".$id =>$comment],200);
+        return response()->json(["COMENTARIOS:"=>$comments],200);
+    }
 }
